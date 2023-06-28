178b27758f5db14da66e025ad1f8bd4d184eb04a
Iewnfod <shangzhiqiuyue@outlook.com>
2023-06-28 09:42:40 +0800

    Add signal to handle Keyboard Interrupt

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from ply import yacc
 from ply import lex
 import sys
 import platform
+import signal
 
 VERSION = 'v0.1.0'
 PLATFORM = f'[ {platform.python_implementation()} {platform.python_version()} ] on {platform.system()}'
@@ -56,6 +57,7 @@ def help():
     print()
 
     print('Options:')
+    arguments.sort()
     for i in arguments:
         print('\t', i[0], '\t', i[1], '\t', i[3])
 
@@ -70,7 +72,7 @@ def version():
     exit(0)
 
 arguments = [  # 输入参数: (参数简写, 参数全称, 运行函数, 描述)
-    ('-gt', '--get-tree', get_tree, 'To show the whole tree of the program'),
+    ('-gt', '--get-tree', get_tree, 'To show the tree of the program after being parsed'),
     ('-v', '--version', version, 'To show the version of this interpreter'),
     ('-h', '--help', help, 'To show this help page')
 ]
@@ -89,11 +91,16 @@ def main():
 
         with_file(path)
 
+def ctrl_c_handle(signal, frame):
+    print('\nKeyboard Interrupt\n$ ', end='')
+
 if __name__ == '__main__':
+    signal.signal(signal.SIGINT, ctrl_c_handle)
+
     lexer = lex.lex()
     parser = yacc.yacc()
 
     try:
         main()
-    except KeyboardInterrupt:
-        print(" Keyboard Interrupt ")
+    except EOFError:
+        print("\nEXIT")
