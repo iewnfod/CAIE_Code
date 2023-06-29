108b1b91be57883b84ae3c3c3b8f16c047d32a9d
Iewnfod <shangzhiqiuyue@outlook.com>
2023-06-29 16:22:50 +0800

    Move all options function into a single file

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 from src.lex import *
 from src.parse import *
 from src.status import *
+from src.options import *
 from ply import yacc
 from ply import lex
 import sys
-import platform
 import signal
 import os
 
-VERSION = 'v0.1.0'
-PLATFORM = f'[ {platform.python_implementation()} {platform.python_version()} ] on {platform.system()}'
-
 preline = '$'
 
+# 清除注释以及多余字符
 def remove_comment(text):
     text = text.split('\n')
     for i in range(len(text)):
@@ -20,15 +18,16 @@ def remove_comment(text):
 
     return '\n'.join(text).strip()
 
+# 预加载文件
+def preload_scripts():
+    for p, dir_list, file_list in os.walk('scripts'):
+        for i in file_list:
+            path = os.path.join(p, i)
+            _, n = os.path.splitext(path)
+            if n == '.cpc':
+                with_file(path, True)
 
-def standard_output():
-    print(f'CAIE Pseudocode Interpreter {VERSION}')
-    print(f'Base on {PLATFORM}')
-    print('Submit issues at https://github.com/iewnfod/CAIE_Code/issues/new')
-    print('Copyright (c) 2023 Iewnfod. ')
-    print('All Rights Reserved. ')
-
-
+# 终端模式，逐行输入并解析运行
 def with_line():
     # 基准输出
     standard_output()
@@ -45,6 +44,7 @@ def with_line():
         except Exception as e:
             print('Error:', e)
 
+# 文件模式，读取文件并解析运行
 def with_file(path, preload=False):
     # 恢复行数，也就是不计算预加载文件的行数
     lexer.lineno = 1
@@ -67,54 +67,12 @@ def with_file(path, preload=False):
         error_messages.insert(0, f'File `{path}`')
         print('\n\t'.join(error_messages))
 
-def preload_scripts():
-    for p, dir_list, file_list in os.walk('scripts'):
-        for i in file_list:
-            path = os.path.join(p, i)
-            _, n = os.path.splitext(path)
-            if n == '.cpc':
-                with_file(path, True)
-
-def help():
-    standard_output()
-
-    print()
-
-    print('Usage: [file_path] [options]')
-
-    print()
-
-    print('Options:')
-    arguments.sort()
-    for i in arguments:
-        print('\t', i[0], '\t', i[1], '\t', i[3])
-
-    exit(0)
-
-def get_tree():
-    global show_tree
-    show_tree = True
-
-def open_debug():
-    global debug
-    debug = True
-
-def version():
-    print('Version:', VERSION)
-    exit(0)
-
-arguments = [  # 输入参数: (参数简写, 参数全称, 运行函数, 描述)
-    ('-gt', '--get-tree', get_tree, 'To show the tree of the program after being parsed'),
-    ('-v', '--version', version, 'To show the version of this interpreter'),
-    ('-h', '--help', help, 'To show this help page'),
-    ('-d', '--debug', open_debug, 'To show debug information during running'),
-]
-
+# 错误的argument
 def wrong_argument(msg):
     print(f'Wrong arguments: \033[1m{msg}\033[0m\n')
     help()
 
-
+# 主函数
 def main():
     # 解析参数
     file_path = ''
@@ -141,9 +99,11 @@ def main():
         else:
             wrong_argument(f'`{file_path}` does not exist')
 
+# 捕获 Keyboard Interrupt
 def ctrl_c_handle(signal, frame):
     print(f'\nKeyboard Interrupt\n{preline} ', end='')
 
+# 程序入口
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, ctrl_c_handle)
 
---
+++ src/options.py
@@ -0,0 +1,47 @@
+import platform
+
+VERSION = 'v0.1.0'
+PLATFORM = f'[ {platform.python_implementation()} {platform.python_version()} ] on {platform.system()}'
+
+def standard_output():
+    print(f'CAIE Pseudocode Interpreter {VERSION}')
+    print(f'Base on {PLATFORM}')
+    print('Submit issues at https://github.com/iewnfod/CAIE_Code/issues/new')
+    print('Copyright (c) 2023 Iewnfod. ')
+    print('All Rights Reserved. ')
+
+def open_debug():
+    global debug
+    debug = True
+
+def version():
+    print('Version:', VERSION)
+    exit(0)
+
+def help():
+    standard_output()
+
+    print()
+
+    print('Usage: [file_path] [options]')
+
+    print()
+
+    print('Options:')
+    arguments.sort()
+    for i in arguments:
+        print('\t', i[0], '\t', i[1], '\t', i[3])
+
+    exit(0)
+
+def get_tree():
+    global show_tree
+    show_tree = True
+
+
+arguments = [  # 输入参数: (参数简写, 参数全称, 运行函数, 描述)
+    ('-gt', '--get-tree', get_tree, 'To show the tree of the program after being parsed'),
+    ('-v', '--version', version, 'To show the version of this interpreter'),
+    ('-h', '--help', help, 'To show this help page'),
+    ('-d', '--debug', open_debug, 'To show debug information during running'),
+]
