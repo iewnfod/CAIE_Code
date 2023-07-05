f69b4b2537537dc40b19be33eb9fd2603d268f32
Iewnfod <shangzhiqiuyue@outlook.com>
2023-07-05 14:21:23 +0800

    Update lsp.py

--- lsp.py
+++ lsp.py
@@ -1,13 +1,32 @@
-from lsprotocol import types
+from lsprotocol.types import *
 from pygls.server import LanguageServer
 
-class cpc_server(LanguageServer):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+from main import *
+from src.AST.data import stack
 
-        self.documents = {}
+cpc_server = LanguageServer('cpc', 'v0.1.0')
 
-    @self.feature(types.INITIALIZE)
-    def initialize(self, params: types.InitializeParams):
-        opts = params.initialization_options
-        method = getattr(opts, 'method', 'builtin')
+@cpc_server.feature(INITIALIZE)
+def initialize(params: InitializeParams):
+    # 预加载文件
+    preload_scripts()
+
+@cpc_server.feature(TEXT_DOCUMENT_COMPLETION)
+def completion(params: CompletionParams):
+    variables = []
+    functions = []
+    for space_name, space_vars, space_functions in stack.spaces:
+        for v in space_vars.keys():
+            variables.append(CompletionItem(label=v))
+        for f in space_functions.keys():
+            functions.append(CompletionItem(label=f))
+
+    return CompletionList(
+        is_incomplete=False,
+        items=variables
+    )
+
+
+# 运行
+def __init__():
+    cpc_server.start_tcp('127.0.0.1', 8080)
