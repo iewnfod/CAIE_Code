3fd5445cd3c3afdac45f875087c26ad0aadc668c
Iewnfod <shangzhiqiuyue@outlook.com>
2023-07-05 14:21:21 +0800

    Remove useless function

--- src/AST/data.py
+++ src/AST/data.py
@@ -2,10 +2,3 @@ from src.stack import Stack
 
 LEVEL_STR = '| '
 stack = Stack()
-
-def check_type_equal(t1, t2):
-    if t1 == t2:
-        return True
-    if (t1 == 'INTEGER' and t2 == 'REAL') or (t1 == 'REAL' and t2 == 'INTEGER'):
-        return True
-    return False
