859603f21038ad9a24a745fc02ab2507e0948a8f
Iewnfod <shangzhiqiuyue@outlook.com>
2023-07-05 19:02:45 +0800

    Bug fix
    修改转义策略
    只有在用双引号包裹的字符串中会发生转义
    input的值并不会参与转义

--- src/AST/data_types.py
+++ src/AST/data_types.py
@@ -29,7 +29,7 @@ class Real(AST_Node):
 class Char(AST_Node):
     def __init__(self, value, *args, **kwargs):
         self.type = 'CHAR'
-        self.value = format(value)
+        self.value = str(value)
         super().__init__(*args, **kwargs)
 
     def get_tree(self, level=0):
@@ -41,7 +41,7 @@ class Char(AST_Node):
 class String(AST_Node):
     def __init__(self, value, *args, **kwargs):
         self.type = 'STRING'
-        self.value = value
+        self.value = str(value).encode('utf8').decode('unicode_escape')
         super().__init__(*args, **kwargs)
 
     def get_tree(self, level=0):
--- src/data_types.py
+++ src/data_types.py
@@ -31,12 +31,12 @@ class REAL(base):
 
 class STRING(base):
     def __init__(self, value=''):
-        self.value = str(value).encode('utf8').decode('unicode_escape')
+        self.value = str(value)
         self.type = 'STRING'
         super().__init__()
 
     def set_value(self, new_value):
-        self.value = str(new_value).encode('utf8').decode('unicode_escape')
+        self.value = str(new_value)
 
     def __str__(self):
         if self.value:
@@ -46,12 +46,12 @@ class STRING(base):
 
 class CHAR(base):
     def __init__(self, value=''):
-        self.value = str(value)[0].encode('utf8').decode('unicode_escape')
+        self.value = str(value)[0]
         self.type = 'CHAR'
         super().__init__()
 
     def set_value(self, new_value):
-        self.value = str(new_value)[0].encode('utf8').decode('unicode_escape')
+        self.value = str(new_value)[0]
 
     def __str__(self):
         if self.value:
