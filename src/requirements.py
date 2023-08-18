5d77259974a756773a891807bc49e1537e683934
Iewnfod <shangzhiqiuyue@outlook.com>
2023-08-18 11:29:45 +0800

    Bug fix
    外部运行无法读取到requirements.txt

--- src/requirements.py
+++ src/requirements.py
@@ -1,20 +1,14 @@
-with open('requirements.txt', 'r') as f:
-    data = f.read().split('\n')
-    data.remove('')
-
 requirements = [
     "ply",
     "chardet",
 ]
-requirements.sort()
 
 def config():
     import importlib
     import pip
-    for i in range(len(requirements)):
-        require = requirements[i]
+    for require in requirements:
         try:
             importlib.import_module(require)
         except:
             print(f'\033[1mMissing Important Dependence `{require}`\nTrying to Install for You...\033[0m')
-            pip.main(['install', data[i]])
+            pip.main(['install', require])
