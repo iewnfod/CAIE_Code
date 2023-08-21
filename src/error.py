df41af50b7259849d1f96423b039f7a6cde0dc7c
Iewnfod <shangzhiqiuyue@outlook.com>
2023-08-21 23:26:37 +0800

    Add color to error

--- src/error.py
+++ src/error.py
@@ -4,11 +4,11 @@ class Error:
         self.ast_obj = ast_obj
 
     def raise_err(self):
-        print(f'Error: {self.message} {self.ast_obj.get_pos()}')
+        print(f'\033[1;31mError\033[0m: \033[1m{self.message}\033[0m {self.ast_obj.get_pos()}')
 
 class StackError:
     def __init__(self, message):
         self.message = str(message)
 
     def raise_err(self):
-        print(f'Stack Error: {self.message}')
+        print(f'\033[1;31mStack Error\033[0m: \033[1m{self.message}\033[0m')
