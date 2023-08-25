f345f16e65ade821b4ef46ed3fc8c67eb28cf5bc
Iewnfod <shangzhiqiuyue@outlook.com>
2023-08-25 11:55:21 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,11 +3,15 @@ echo "安装 CAIE Code 中..."
 current_dir=$(cd $(dirname $0); pwd)
 # 合成写入内容
 content="export PATH=${current_dir}/bin:\$PATH"
-# 写入文件
+# 写入 bash_profile 文件
 echo "" >> ~/.bash_profile  # 换行
 echo "# CAIE Code Binary Environment" >> ~/.bash_profile
 echo $content >> ~/.bash_profile
+# 写入 zshrc 文件
+echo "" >> ~/.zshrc  # 换行
+echo "# CAIE Code Binary Environment" >> ~/.zshrc
+echo $content >> ~/.zshrc
 
 echo "安装完成"
-echo "请重启终端使新的PATH生效"
+echo "请重启终端使配置生效"
 echo "若移动了当前目录，请重新运行此文件"
