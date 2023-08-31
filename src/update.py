d9ae5908bcf9f755d434c3c18f4549395cadcfe1
Iewnfod <shangzhiqiuyue@outlook.com>
2023-08-31 20:21:05 +0800

    Add Update Animation

--- src/update.py
+++ src/update.py
@@ -1,8 +1,11 @@
 from .history import HOME_PATH
 import os
 import git
+import time
+import threading
 
 VERSION = ''
+flags = {}
 
 with open(os.path.join(HOME_PATH, 'VERSION'), 'r') as f:
     VERSION = f.read().strip()
@@ -14,18 +17,34 @@ def check_update(repo: git.Repo, remote: git.Remote):
     remote_branch = repo.remotes.origin.refs[local_branch.name]
     return local_branch.commit != remote_branch.commit
 
+
+def animation(msg, count):
+    count += 1
+    n = 1
+    while flags[msg]:
+        print(msg + '.' * n, end='\r')
+        n += 1
+        n %= count
+        time.sleep(.5)
+
+def new_animation(msg, count, work, *args, **kwargs):
+    flags[msg] = True
+    threading.Thread(target=animation, args=(msg, count)).start()
+    result = work(*args, **kwargs)
+    flags[msg] = False
+    return result
+
+
 def update():
     repo = git.Repo(HOME_PATH)
     remote = repo.remote()
 
-    print('Checking Update...', end='\r')
-    if check_update(repo, remote):
+    if new_animation('Checking Update', 3, check_update, repo=repo, remote=remote):
         # 询问是否更新
         u = input('There is a new version of the program. Do you want to update it? [y/N] ').strip().lower()
         if u == 'y':
             try:
-                print('Updating')
-                remote.pull()
+                new_animation('Updating', 3, remote.pull)
                 print('\033[1mUpdate Successful\033[0m')
             except Exception as e:
                 print(f'\033[1;31mFailed to Update\033[0m\n\t{e}')
