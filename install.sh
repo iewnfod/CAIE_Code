a73244f2676e5e1d1f4e93706c44be142d96d0c6
Iewnfod <shangzhiqiuyue@outlook.com>
2023-09-05 12:51:06 +0800

    Add fish install

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ echo "安装 CAIE Code 中..."
 current_dir=$(cd $(dirname $0); pwd)
 # 合成写入内容
 content="export PATH=${current_dir}/bin:\$PATH"
+fish_content="set -x PATH \"${current_dir}/bin\" \$PATH"
 # 写入 bash_profile 文件
 echo "" >> ~/.bash_profile  # 换行
 echo "# CAIE Code Binary Environment" >> ~/.bash_profile
@@ -11,6 +12,10 @@ echo $content >> ~/.bash_profile
 echo "" >> ~/.zshrc  # 换行
 echo "# CAIE Code Binary Environment" >> ~/.zshrc
 echo $content >> ~/.zshrc
+# 写入 fish 配置文件
+echo "" >> ~/.config/fish/config.fish
+echo "# CAIE Code Binary Environment" >> ~/.config/fish/config.fish
+echo $fish_content >> ~/.config/fish/config.fish
 
 echo "安装完成"
 echo "请重启终端使配置生效"
