f681488bdae10c7092606e83b303f110f541d7e7
Iewnfod <shangzhiqiuyue@outlook.com>
2023-09-08 18:39:12 +0800

    Bug fix: logic calculation error
    当出现类似于 'A' OR 'B' 这种逻辑运算时，会出现不必要的错误

--- src/AST/logic.py
+++ src/AST/logic.py
@@ -13,7 +13,7 @@ class Logic_and(AST_Node):
         return LEVEL_STR * level + self.type + '\n' + self.left.get_tree(level+1) + '\n' + self.right.get_tree(level+1)
 
     def exe(self):
-        return (self.left.exe()[0] and self.right.exe()[0], 'BOOLEAN')
+        return (bool(self.left.exe()[0] and self.right.exe()[0]), 'BOOLEAN')
 
 class Logic_or(AST_Node):
     def __init__(self, left, right, *args, **kwargs):
@@ -26,7 +26,7 @@ class Logic_or(AST_Node):
         return LEVEL_STR * level + self.type + '\n' + self.left.get_tree(level+1) + '\n' + self.right.get_tree(level+1)
 
     def exe(self):
-        return (self.left.exe()[0] or self.right.exe()[0], 'BOOLEAN')
+        return (bool(self.left.exe()[0] or self.right.exe()[0]), 'BOOLEAN')
 
 class Logic_not(AST_Node):
     def __init__(self, value, *args, **kwargs):
@@ -38,4 +38,4 @@ class Logic_not(AST_Node):
         return LEVEL_STR * level + self.type + '\n' + self.value.get_tree(level+1)
 
     def exe(self):
-        return (not self.value.exe()[0], 'BOOLEAN')
+        return (bool(not self.value.exe()[0]), 'BOOLEAN')
