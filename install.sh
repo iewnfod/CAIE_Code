64c7255cd3acd1c464f384e40e18911392d5df75
Iewnfod <shangzhiqiuyue@outlook.com>
2023-10-16 20:01:40 +0800

    Add type test on install.sh

--- install.sh
+++ install.sh
@@ -6,17 +6,23 @@ current_dir=$(cd $(dirname $0); pwd)
 content="export PATH=${current_dir}/bin:\$PATH"
 fish_content="set -x PATH \"${current_dir}/bin\" \$PATH"
 # 写入 bash_profile 文件
-echo "" >> ~/.bash_profile  # 换行
-echo "# CAIE Code Binary Environment" >> ~/.bash_profile
-echo $content >> ~/.bash_profile
+if type bash >/dev/null 2>&1; then
+	echo "" >> ~/.bash_profile  # 换行
+	echo "# CAIE Code Binary Environment" >> ~/.bash_profile
+	echo $content >> ~/.bash_profile
+fi
 # 写入 zshrc 文件
-echo "" >> ~/.zshrc  # 换行
-echo "# CAIE Code Binary Environment" >> ~/.zshrc
-echo $content >> ~/.zshrc
+if type zsh >/dev/null 2>&1; then
+	echo "" >> ~/.zshrc  # 换行
+	echo "# CAIE Code Binary Environment" >> ~/.zshrc
+	echo $content >> ~/.zshrc
+fi
 # 写入 fish 配置文件
-echo "" >> ~/.config/fish/config.fish
-echo "# CAIE Code Binary Environment" >> ~/.config/fish/config.fish
-echo $fish_content >> ~/.config/fish/config.fish
+if type fish >/dev/null 2>&1; then
+	echo "" >> ~/.config/fish/config.fish
+	echo "# CAIE Code Binary Environment" >> ~/.config/fish/config.fish
+	echo $fish_content >> ~/.config/fish/config.fish
+fi
 # 链接到 man 目录
 sudo ln -f ${current_dir}/man/cpc.1 /usr/local/share/man/man1/cpc.1
 
