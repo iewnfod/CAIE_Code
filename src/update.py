cefaebb566610887ed0f843ff1d346818d64f5e3
lightumcc <jessiezhu@createchstudio.com>
2023-11-08 08:38:47 +0800

    fix: Unexpected whitespace
    我是傻逼（顺便测试）

--- src/update.py
+++ src/update.py
@@ -31,11 +31,11 @@ def update():
 
     if new_animation('Checking Update', 3, check_update, failed_msg='Failed to Check Update', repo=repo, remote=remote):
         # 询问是否更新
-        u = input(f'There is a new version of the program({latest_commit_hash}: {latest_commit_message}).\n Do you want to update it? [Y/n] ').strip().lower()
+        u = input(f'There is a new version of the program({latest_commit_hash}: {latest_commit_message}).\nDo you want to update it? [Y/n] ').strip().lower()
         if u == '' or u == 'y':
             if new_animation('Updating', 3, _update, failed_msg='Failed to Update', remote=remote, repo=repo):
                 print('\033[1mUpdate Successful\033[0m')
         else:
             print('Stop Updating')
     else:
-        print(f'You are using the latest version!\n At {latest_commit_hash}: {latest_commit_message}')
+        print(f'You are using the latest version!\nAt {latest_commit_hash}: {latest_commit_message}')
