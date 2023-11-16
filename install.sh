a24aeea7e64664bd95c6430310ad045aa59d23eb
lightumcc <jessiezhu@createchstudio.com>
2023-11-16 22:29:44 +0800

    update: installation script

--- install.sh
+++ install.sh
@@ -1,40 +1,22 @@
+set -e
+
+exit_install_failure() {
+    echo "安装失败"
+    exit 1
+}
+
 echo "安装 CAIE Code 中... (此过程中可能会需要sudo权限)"
 # 获取当前执行目录
 current_dir=$(cd $(dirname $0); pwd)
-# 生成 man : pandoc man/cpc.md -s -t man -o man/cpc.1
-# 合成写入内容
-# content="export PATH=${current_dir}/bin:\$PATH"
-# fish_content="set -x PATH \"${current_dir}/bin\" \$PATH"
-# 如果已经可以用 cpc 了，那就不用重新写入了
-# if ! type cpc >/dev/null 2>&1; then
-# 	# 写入 bash_profile 文件
-# 	if type bash >/dev/null 2>&1; then
-# 		echo "" >> ~/.bash_profile  # 换行
-# 		echo "# CAIE Code Binary Environment" >> ~/.bash_profile
-# 		echo $content >> ~/.bash_profile
-# 	fi
-# 	# 写入 zshrc 文件
-# 	if type zsh >/dev/null 2>&1; then
-# 		echo "" >> ~/.zshrc  # 换行
-# 		echo "# CAIE Code Binary Environment" >> ~/.zshrc
-# 		echo $content >> ~/.zshrc
-# 	fi
-# 	# 写入 fish 配置文件
-# 	if type fish >/dev/null 2>&1; then
-# 		echo "" >> ~/.config/fish/config.fish
-# 		echo "# CAIE Code Binary Environment" >> ~/.config/fish/config.fish
-# 		echo $fish_content >> ~/.config/fish/config.fish
-# 	fi
-# fi
+loggedInUser=$(/bin/ls -l /dev/console | /usr/bin/awk '{ print $3 }')
+
+git config --global --add safe.directory ${current_dir} || exit_install_failure
 
 # 链接到 bin 目录
-if [ $(arch) = "arm64" ]; then
-	sudo ln -sf ${current_dir}/bin/cpc_arm /usr/local/bin/cpc
-elif [ $(arch) = "x86_64" ]; then
-	sudo ln -sf ${current_dir}/bin/cpc_x86 /usr/local/bin/cpc
-fi
+sudo ln -sf ${current_dir}/bin/cpc /usr/local/bin/cpc || exit_install_failure
+
 # 链接到 man 目录
-sudo ln -f ${current_dir}/man/cpc.1 /usr/local/share/man/man1/cpc.1
+sudo ln -f ${current_dir}/man/cpc.1 /usr/local/share/man/man1/cpc.1 || exit_install_failure
 
 echo "安装完成"
 echo "若移动了当前目录，请重新运行此文件"
