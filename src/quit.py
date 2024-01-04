ecd080ff631c9e1c07bff9358bd46e595bf31918
lightumcc <jessiezhu@createchstudio.com>
2024-01-04 10:57:31 +0800

    fix: wrong cursor position with zsh after exit

--- src/quit.py
+++ src/quit.py
@@ -1,10 +1,10 @@
-from os import _exit
 from .global_var import output_error, console
 from .AST import stack
+from sys import exit
 
 def quit(code=0):
     stack.close_all_files()
     output_error()
     console.postloop()
     stack.delete()
-    _exit(code)
+    exit(code)
